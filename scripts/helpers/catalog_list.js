6ee5baf13c1e4fea633e3d517cb0df44dba8eeb4
Eurkon <1124724692@qq.com>
2022-03-17 13:14:51 +0800

    improvement: 分类标签条优化

--- scripts/helpers/catalog_list.js
+++ scripts/helpers/catalog_list.js
@@ -3,7 +3,7 @@ hexo.extend.helper.register('catalog_list', function (type) {
   const theme = hexo.theme.config
   hexo.locals.get(type).map(function (item) {
     html += `
-    <div class="catalog-list-item" id="${item.name}">
+    <div class="catalog-list-item" id="${type + item.name}">
       <a href="/${type}/${item.name}/">${theme.emoji[item.name] + [item.name]}<sup>${item.length}</sup></a>
     </div>
     `
