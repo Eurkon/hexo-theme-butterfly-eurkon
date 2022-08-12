42644554334b2762cc069bfdd36cabdf31c057c7
Eurkon <1124724692@qq.com>
2022-08-12 10:17:44 +0800

    update: random.js

--- scripts/helpers/random.js
+++ scripts/helpers/random.js
@@ -7,14 +7,14 @@ hexo.extend.filter.register('after_render:html', function (data) {
   return data
 })
 
-hexo.extend.generator.register('random', function (locals) {
-  const config = hexo.config.random || {}
-  const posts = []
-  for (const post of locals.posts.data) {
-    if (post.random !== false) posts.push(post.path)
-  }
-  return {
-    path: config.path || 'random/index.html',
-    data: `<html><head><script>var posts=${JSON.stringify(posts)}; window.pjax ? pjax.loadUrl('/'+posts[Math.floor(Math.random()*posts.length)]) : window.open('/'+posts[Math.floor(Math.random()*posts.length)], "_self");</script></head></html>`
-  }
-})
+// hexo.extend.generator.register('random', function (locals) {
+//   const config = hexo.config.random || {}
+//   const posts = []
+//   for (const post of locals.posts.data) {
+//     if (post.random !== false) posts.push(post.path)
+//   }
+//   return {
+//     path: config.path || 'random/index.html',
+//     data: `<html><head><script>var posts=${JSON.stringify(posts)}; window.pjax ? pjax.loadUrl('/'+posts[Math.floor(Math.random()*posts.length)]) : window.open('/'+posts[Math.floor(Math.random()*posts.length)], "_self");</script></head></html>`
+//   }
+// })
