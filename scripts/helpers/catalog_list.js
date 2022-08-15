fab8f97d884a69041f9598bc62acd5cce0dd12ef
Eurkon <1124724692@qq.com>
2022-08-15 16:59:02 +0800

    improvement: 分类标签导航栏适配多分类文章

--- scripts/helpers/catalog_list.js
+++ scripts/helpers/catalog_list.js
@@ -3,8 +3,8 @@ hexo.extend.helper.register('catalog_list', function (type) {
   const theme = hexo.theme.config
   hexo.locals.get(type).map(function (item) {
     html += `
-    <div class="catalog-list-item" id="${type + '-' + item.name}">
-      <a href="/${type}/${item.name}/">${theme.emoji[item.name] + item.name}<sup>${item.length}</sup></a>
+    <div class="catalog-list-item" id="/${item.path}">
+      <a href="/${item.path}">${theme.emoji[item.name] + item.name}<sup>${item.length}</sup></a>
     </div>
     `
   })
