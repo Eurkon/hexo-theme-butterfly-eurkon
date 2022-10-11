f7fa991856f4a7f70f80f048a5e297d7b4d057d5
Eurkon <1124724692@qq.com>
2022-10-11 17:46:01 +0800

    feat: rightside 按钮使用导航栏设置控制

--- source/js/eurkon/refresh.js
+++ source/js/eurkon/refresh.js
@@ -4,3 +4,5 @@ eurkon.catalogActive()
 
 // 顶部菜单栏 评论按钮
 if (document.getElementById('comment-button')) document.getElementById('comment-button').style.display = document.getElementById('post-comment') ? 'block' : 'none'
+if (document.getElementById('setting-button') && document.getElementById('rightside')) document.getElementById('setting-button').onclick = function () { document.getElementById('rightside').classList.remove('hidden') }
+if (document.getElementById('rightside')) document.getElementById('rightside').onclick = function () { document.getElementById('rightside').classList.add('hidden') }
