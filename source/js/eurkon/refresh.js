c86fd442d9a0fa476f08db0a82edebbb12a40c7d
Eurkon <1124724692@qq.com>
2023-01-29 11:28:37 +0800

    feat: 页面滚动百分比

--- source/js/eurkon/refresh.js
+++ source/js/eurkon/refresh.js
@@ -2,6 +2,7 @@ eurkon.switchThemeColor(eurkon.getMainColor())
 eurkon.switchPageTitle()
 eurkon.catalogActive()
 eurkon.postAddToc()
+eurkon.footerRandomFlink(flinks, 3)
 
 // 顶部菜单栏 评论按钮
 if (document.getElementById('comment-button')) document.getElementById('comment-button').style.display = document.getElementById('post-comment') ? 'block' : 'none'
@@ -18,4 +19,11 @@ document.addEventListener('keydown', function (event) {
     }
   }
 });
+
+// 页面滚动百分比
+window.addEventListener('scroll', function () {
+  let totalH = document.body.scrollHeight || document.documentElement.scrollHeight // 页面总高
+  let clientH = window.innerHeight || document.documentElement.clientHeight // 可视高
+  document.querySelector('#nav #hotkey #top-button a.site-page i').dataset.percent = ((document.body.scrollTop || document.documentElement.scrollTop) / (totalH - clientH) * 100).toFixed(0)
+})
 
