d3cf916faed5ba83249ffdaf42eaa90c54f96d9f
Eurkon <1124724692@qq.com>
2023-02-21 09:57:09 +0800

    fix: 修复无友链文件导致的错误

--- scripts/helpers/random.js
+++ scripts/helpers/random.js
@@ -1,22 +1,23 @@
 hexo.extend.filter.register('after_render:html', function (data) {
-  const posts = []
-  hexo.locals.get('posts').map(function (post) {
-    if (post.random !== false) posts.push(post.path)
-  })
-  data += `<script>var posts=${JSON.stringify(posts)};function toRandomPost(){ window.pjax ? pjax.loadUrl('/'+posts[Math.floor(Math.random()*posts.length)]) : window.open('/'+posts[Math.floor(Math.random()*posts.length)], "_self"); };</script>`
-  return data
+  try {
+    const posts = []
+    hexo.locals.get('posts').forEach(function (post) {
+      if (post.random !== false) posts.push(post.path)
+    })
+    data += `<script>var posts=${JSON.stringify(posts)};function toRandomPost(){ window.pjax ? pjax.loadUrl('/'+posts[Math.floor(Math.random()*posts.length)]) : window.open('/'+posts[Math.floor(Math.random()*posts.length)], "_self"); };</script>`
+  } catch (e) { } finally { return data }
 })
 
 hexo.extend.filter.register('after_render:html', function (data) {
-  const flinks = []
-  hexo.locals.get('data').link.map(function (list) {
-    list.link_list.map(function (flink) {
-      flinks.push(flink)
+  try {
+    const flinks = []
+    hexo.locals.get('data').link.forEach(function (list) {
+      list.link_list.map(function (flink) {
+        flinks.push(flink)
+      })
     })
-  })
-
-  data += `<script>var flinks=${JSON.stringify(flinks)};function toRandomFlink(){window.open(flinks[Math.floor(Math.random()*flinks.length)].link);};</script>`
-  return data
+    data += `<script>var flinks=${JSON.stringify(flinks)};function toRandomFlink(){window.open(flinks[Math.floor(Math.random()*flinks.length)].link);};</script>`
+  } catch (e) { } finally { return data }
 })
 
 // hexo.extend.generator.register('random', function (locals) {
