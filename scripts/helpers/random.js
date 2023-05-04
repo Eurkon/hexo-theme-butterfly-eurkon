dbcd2155892450d181f324bc1fd52f5ab4147e77
Eurkon <1124724692@qq.com>
2023-05-04 14:45:42 +0800

    pref: 随机文章

--- scripts/helpers/random.js
+++ scripts/helpers/random.js
@@ -2,9 +2,22 @@ hexo.extend.filter.register('after_render:html', function (data) {
   try {
     const posts = []
     hexo.locals.get('posts').forEach(function (post) {
-      if (post.random !== false) posts.push(post.path)
+      if (post.random !== false) {
+        posts.push({
+          'title': post.title,
+          'path': '/' + post.path,
+          'categories': post.categories.data.map(function (item) { return { 'name': item.name, 'path': '/' + item.path } }),
+          'tags': post.tags.data.map(function (item) { return { 'name': item.name, 'path': '/' + item.path } }),
+          'cover': post.cover,
+          // 'description': post.description,
+          // 'wordCount': post.content.replace(/<(?:.|\s)*?>/g, '').length,
+          // 'content': strip_html(post.content),
+          'date': post.date,
+          'updated': post.updated
+        })
+      }
     })
-    data += `<script>var posts=${JSON.stringify(posts)};function toRandomPost(){ window.pjax ? pjax.loadUrl('/'+posts[Math.floor(Math.random()*posts.length)]) : window.open('/'+posts[Math.floor(Math.random()*posts.length)], "_self"); };</script>`
+    data += `<script>var posts=${JSON.stringify(posts)};function toRandomPost(){ window.pjax ? pjax.loadUrl(posts[Math.floor(Math.random()*posts.length)].path) : window.open(posts[Math.floor(Math.random()*posts.length)].path, "_self"); };</script>`
   } catch (e) { } finally { return data }
 })
 
@@ -19,15 +32,3 @@ hexo.extend.filter.register('after_render:html', function (data) {
     data += `<script>var flinks=${JSON.stringify(flinks)};function toRandomFlink(){window.open(flinks[Math.floor(Math.random()*flinks.length)].link);};</script>`
   } catch (e) { } finally { return data }
 })
-
-// hexo.extend.generator.register('random', function (locals) {
-//   const config = hexo.config.random || {}
-//   const posts = []
-//   for (const post of locals.posts.data) {
-//     if (post.random !== false) posts.push(post.path)
-//   }
-//   return {
-//     path: config.path || 'random/index.html',
-//     data: `<html><head><script>var posts=${JSON.stringify(posts)}; window.pjax ? pjax.loadUrl('/'+posts[Math.floor(Math.random()*posts.length)]) : window.open('/'+posts[Math.floor(Math.random()*posts.length)], "_self");</script></head></html>`
-//   }
-// })
